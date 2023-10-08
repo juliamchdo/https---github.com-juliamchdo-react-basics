ee219249af7a1aea5b94fecded8770d8914a3af1
julia <juliamachado226@gmail.com>
2023-10-08 11:38:14 -0300

    componentização dos posts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
+import { PostCard } from "./components/PostCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    photos: [],
+    posts: [],
+  };
+
+  timeoutUpdate = null;
+
+  //lifecycle methods//
+  componentDidMount() {
+    this.loadPosts();
+  }
+  //lifecycle methods//
+
+  loadPosts = async () => {
+    const postsResponse = fetch("https://jsonplaceholder.typicode.com/posts");
+    const photosResponse = fetch("https://jsonplaceholder.typicode.com/photos");
+
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    //como tem mais photos do que posts, retorna a quantidade exata
+    const photosAndPosts = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url };
+    });
+
+    this.setState({ posts: photosAndPosts });
+
+    // fetch('https://jsonplaceholder.typicode.com/posts')
+    // .then(response => response.json())
+    // .then(posts => this.setState({ posts }))
+  };
+
+  render() {
+    //destructuring
+    const { posts } = this.state;
+
+    return (
+      <section className="container">
+        <div className="posts">
+          {posts.map((post) => (
+            <PostCard
+              key={post.id}
+              title={post.title}
+              body={post.body}
+              cover={post.cover}
+              id={post.id}
+              />
+          ))}
+        </div>
+      </section>
+    );
+  }
 }
 
 export default App;
